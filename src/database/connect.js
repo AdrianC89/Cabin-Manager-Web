2e8fb1a10c83756d346b990239a052189a9da635
Ricardo Alvarez <ricardo.alvarez@normaltandil.edu.ar>
2024-07-24 20:52:13 -0300

    Nuevo .ENV

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,9 +1,13 @@
+// config/database.js
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('cabin-manager', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
+dotenv.config(); // Cargar las variables de entorno desde el archivo .env
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
     logging: false, // Deshabilitar el logging para limpiar la salida de la consola
 });
 
